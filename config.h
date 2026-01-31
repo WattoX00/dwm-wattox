57a38562d9f3a1f55653e4aaad1d608e53440858
Wattox00 <korschzozinho@gmail.com>
2026-01-31 18:14:12 +0000

    Font fix

--- config.h
+++ config.h
@@ -13,14 +13,14 @@ static const int systraypinningfailfirst      = 1;   /* 1: if pinning fails, dis
 static const int showsystray                  = 0;   /* 0 means no systray - disable when using Polybar */
 static const int showbar                      = 1;   /* 1 means show bar - needed for Polybar space calculation */
 static const int topbar                       = 1;   /* 0 means bottom bar */
-static const int usealtbar                     = 1;   /* 1 means use non-dwm status bar */
-static const char *altbarclass                 = "Polybar"; /* Alternate bar class name */
-static const char *alttrayname                 = "tray";    /* Polybar tray instance name */
-static const char *altbarcmd                   = "$HOME/.config/polybar/launch.sh";
+static const int usealtbar                    = 1;   /* 1 means use non-dwm status bar */
+static const char *altbarclass                = "Polybar"; /* Alternate bar class name */
+static const char *alttrayname                = "tray";    /* Polybar tray instance name */
+static const char *altbarcmd                  = "$HOME/.config/polybar/launch.sh";
 #define ICONSIZE                              17     /* icon size */
 #define ICONSPACING                           5      /* space between icon and title */
 #define SHOWWINICON                           1      /* 0 means no winicon */
-static const char *fonts[]                    = { "MesloLGS Nerd Font Mono:size=14:antialias=true:autohint=true:hintstyle=hintfull", "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true" };
+static const char *fonts[]                    = { "MesloLGS Nerd Font Mono:size=14:antialias=true:autohint=true:hintstyle=hintfull", "Source Han Sans JP:size=14", "Noto Color Emoji:pixelsize=16" };
 static const char normbordercolor[]           = "#3B4252";
 static const char normbgcolor[]               = "#434C5E";  /* Lighter background for better icon contrast */
 static const char normfgcolor[]               = "#D8DEE9";
