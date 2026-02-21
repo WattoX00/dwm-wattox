67c759f581b2f786d65e8556b4e4649b87d5eebf
wattox00 <korschzozinho@gmail.com>
2026-02-21 19:34:24 +0100

    Static rules cleaned up

--- config.h
+++ config.h
@@ -40,8 +40,9 @@ static const char *const autostart[] = {
     "flameshot", NULL,
     "dunst", NULL,
     "picom", "-b", NULL,
+    "nm-applet", NULL,
     "sh", "-c", "feh --randomize --bg-fill ~/Pictures/backgrounds/*", NULL,
-    NULL /* terminate */
+    NULL
 };
 
 /* tagging */
@@ -52,12 +53,7 @@ static const int lcaselbl = 0;          /* 1 means make tag label lowercase */
 
 static const Rule rules[] = {
     /* class                instance  title           tags mask  isfloating  isterminal  noswallow  monitor  */
-    { "St",                 NULL,     NULL,           0,         0,          1,          0,        -1 },
-    { "kitty",              NULL,     NULL,           0,         0,          1,          0,        -1 },
     { "alacritty",          NULL,     NULL,           0,         0,          1,          0,        -1 },
-    { "ghostty",            NULL,     NULL,           0,         0,          1,          0,        -1 },
-    { "warp-terminal",      NULL,     NULL,           0,         0,          1,          0,        -1 },
-    { "terminator",         NULL,     NULL,           0,         0,          1,          0,        -1 },
     { "thunar",             NULL,     NULL,           0,         0,          0,          0,        -1 },
     { NULL,                 NULL,     "Event Tester", 0,         0,          0,          1,        -1 }, /* xev */
 };
